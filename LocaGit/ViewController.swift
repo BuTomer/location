aa2a714537e275fc410b3ecbc71317117005caa8
BuTomer <bu.tomer@gmail.com>
2021-12-20 12:11:07 +0200

    initial commit

---
+++ LocaGit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LocaGit
+//
+//  Created by Tomer Buzaglo 
+//  
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
